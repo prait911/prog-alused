fa1f52e73df11c34b6550fdb2677c26daf636fd2
Your Name <prait.kirilo@voco.ee>
2024-10-09 09:51:56 +0300

    muutujad

---
+++ variables.py
@@ -0,0 +1,9 @@
+word = "spider"
+thousand = "1000"
+ten = 10
+first_half = "full"
+second_half = "moon"
+complete = "full moon"
+complete = first_half + second_half
+full_name_with_space = first_half + " " + second_half
+print(complete)
