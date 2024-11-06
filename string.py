dfa5b62a2430450b1b64a3868e1d32b27b1b7c4c
Your Name <prait.kirilo@voco.ee>
2024-11-06 09:18:23 +0200

    string.py

---
+++ string.py
@@ -0,0 +1,17 @@
+first_name = "James"
+last_name = "Bond"
+full_name = first_name + " " + last_name
+self_description_sentence = "My name is " + last_name + ", " + first_name + " " + last_name + "."
+
+cake = "vahukoor\nmarjad\ntäidis\npõhi"
+print(cake)
+
+original_string = "Programming is fun!"
+backwards = original_string[::-1]
+# print(backwards)
+
+every_other = original_string[::2]
+# print(every_other)
+
+first_word_reversed = original_string[10::-1]
+# print(first_word_reversed)
