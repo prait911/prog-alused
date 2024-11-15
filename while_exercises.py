2948fe9fa458e26f81a2cd687e5a5fb5b4d427be
Your Name <prait.kirilo@voco.ee>
2024-11-15 09:10:39 +0200

    while_exercises.py

---
+++ while_exercises.py
@@ -0,0 +1,91 @@
+import random
+
+
+def make_hola_string(count: int) -> str:
+    """
+    Make hola string.
+    
+    print(make_hola_string(3)) => "holaholahola"
+    print(make_hola_string(0)) => ""
+    """
+    # Your code goes here
+    sum = ""
+    for i in range(int(count)):
+        sum += "hola"
+    return sum
+print(make_hola_string(4))
+
+def generate_string_with_random_length(threshold: float) -> str:
+    """
+    Generate a string of "-" until random numbers is below threshold.
+    
+    Use random.random() to generate a random float.
+    If the random number is below threshold, add "-" to result.
+    If the random number is greater or equal to the threshold, finish the loop.
+
+    generate_string_with_random_length(0.9) => "-----" (result can vary)
+    generate_string_with_random_length(0.5) => "-" (usually empty or 1 minus)
+    """
+    # code here
+
+    juhuarv = random.random()
+    sum = ""
+    while juhuarv < threshold:
+        sum += "-"
+        juhuarv = random.random()
+    return sum
+    
+print(generate_string_with_random_length(0.9))
+
+
+def ask_user_age(age_limit: int) -> int:
+    """
+    Ask user age.
+
+    You have to ask the user his/her age using input("What is your age?").
+    You have to repeat this process until a correct age is entered.
+    The age is correct if:
+    - it is numeric (answering "a" is not correct)
+    - it is greater or equal to the age_limit
+    - it is less or equal to 100
+
+    So, if the user enters a wrong age, the user gets a warning.
+    The question is repeated until a correct age is entered.
+    The function returns the correct age as int.
+
+    Warning is printed out:
+    - non numberic input: Wrong input!
+    - age < age_limit: Too young!
+    - age > 100: Too old!
+
+    An example (with age_limit 18):
+    What is your age? a
+    Wrong input!
+    What is your age? 10
+    Too young!
+    What is your age? 101
+    Too old!
+    What is your age? 21
+
+    (function returns 21)
+    """
+
+    
+    while True:
+        age_input = input("what is your age")
+        
+        if age_input.isdigit():
+            print("wrong input!")
+        if age_input < 18:
+            print("too young")
+        elif age_input > 100:
+            print("too old")
+        return age_input
+        
+    
+    
+    
+    
+    
+    
+    
